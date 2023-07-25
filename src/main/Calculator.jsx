660ac1831745b82122284188e4b7fab42550c1df
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2023-07-25 16:51:30 -0300

    Componente Calulator

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import './Calculator.css'
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            // é importante usar "className" no react
+            <div className="calculator">
+
+            </div>
+        )
+    }
+}
