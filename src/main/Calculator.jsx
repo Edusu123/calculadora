18e740a7a4a4a5534c1c9a2e28f680e2532e3ff8
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2023-07-26 13:51:58 -0300

    Componente display

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,12 +1,14 @@
 import './Calculator.css'
 import React, { Component } from "react";
 import Button from "../components/Button";
+import Display from '../components/Display';
 
 export default class Calculator extends Component {
     render() {
         return (
             // é importante usar "className" no react
             <div className="calculator">
+                <Display value={100}></Display>
                 <Button label="AC"></Button>
                 <Button label="/"></Button>
                 <Button label="7"></Button>
